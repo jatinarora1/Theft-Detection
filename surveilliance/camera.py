7d5d6c0f8cbd4ea5bdcb3f6841cdd27a5edf90a3
jatinarora1 <arora8331@gmail.com>
2020-07-30 14:02:55 +0530

    new updates

--- surveilliance/camera.py
+++ surveilliance/camera.py
@@ -1,49 +1,35 @@
 import urllib.request
 import cv2
-url = 0
+
+url = [0,'http://100.72.226.182:8080/video','http://100.72.226.182:8080/video']
 ds_factor=0.6
-cap = cv2.VideoCapture(url)
+cap = cv2.VideoCapture(url[1])
 class VideoCamera(object):
     def __init__(self):
        #capturing video
-       self.video = cv2.VideoCapture(url)
+       self.video1 = cv2.VideoCapture(url[0])
+       self.video2 = cv2.VideoCapture(url[1])
+       self.video3 = cv2.VideoCapture(url[2])
       
     def __del__(self):
         #releasing camera
-        self.video.release()
+        self.video1.release()
+        self.video2.release()
+        self.video3.release()
+        
     def get_frame(self):
       #extracting frames
-      _, frame1 = self.video.read()
-      _, frame2 = self.video.read()
+      _, frame1 = self.video1.read()
+      _, frame2 = self.video2.read()
+      _, frame3 = self.video3.read()
       frame1=cv2.resize(frame1,None,fx=ds_factor,fy=ds_factor,
       interpolation=cv2.INTER_AREA)
       frame2=cv2.resize(frame2,None,fx=ds_factor,fy=ds_factor,
       interpolation=cv2.INTER_AREA)
-
-      diff = cv2.absdiff(frame1,frame2)
-      gray = cv2.cvtColor(diff,cv2.COLOR_RGB2GRAY)
-      blur = cv2.GaussianBlur(gray,(5,5),0)
-      blurred = cv2.GaussianBlur(cv2.cvtColor(frame1,cv2.COLOR_RGB2GRAY),(5,5),0)
-      _,thresh = cv2.threshold(blur,20,255,cv2.THRESH_BINARY)
-      dilated = cv2.dilate(thresh,None,iterations = 3)
-      (cnts,_)=cv2.findContours(dilated.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-      
-
-      for contour in cnts:
-        if cv2.contourArea(contour) < 10000:
-            continue
-
-        
-        (x, y, w, h)=cv2.boundingRect(contour)
-        cv2.rectangle(frame1, (x, y), (x+w, y+h), (0,255,0), 2)
-      
-      frame1 = frame2
-      #     text = "Occupied"
-      ret, jpeg= cv2.imencode('.jpg', frame1)
-      ret, jpeg2 = cv2.imencode('.jpg',gray)
-      ret, jpeg3 = cv2.imencode('.jpg',blurred)
-      ret, jpeg4 = cv2.imencode('.jpg',dilated)
-
-      _,frame2 = self.video.read()
-      return jpeg.tobytes(),jpeg2.tobytes(),jpeg3.tobytes(),jpeg4.tobytes()
+      frame3=cv2.resize(frame3,None,fx=ds_factor,fy=ds_factor,
+      interpolation=cv2.INTER_AREA)
+      ret, jpeg1 = cv2.imencode('.jpg',frame1)
+      ret, jpeg2 = cv2.imencode('.jpg',frame2)
+      ret, jpeg3 = cv2.imencode('.jpg',frame3)
+      return jpeg1.tobytes(), jpeg2.tobytes(), jpeg3.tobytes()
  
--- surveilliance/main.py
+++ surveilliance/main.py
@@ -1,37 +1,27 @@
-# main.py
-# import the necessary packages
 from flask import Flask, render_template, Response
 from camera import VideoCamera
 app = Flask(__name__)
 @app.route('/')
 def index():
-    # rendering webpage
     return render_template('index.html')
 def gen(camera):
     while True:
         #get camera frame
-        frame,_,_,_ = camera.get_frame()
+        frame,_,_= camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 def gen1(camera):
     while True:
         #get camera frame
-        _,gray,_,_ = camera.get_frame()
+        _,frame2,_ = camera.get_frame()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + gray + b'\r\n\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame2 + b'\r\n\r\n')
 def gen2(camera):
     while True:
         #get camera frame
-        _,_,delta,_ = camera.get_frame()
+        _,_,frame3 = camera.get_frame()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + delta + b'\r\n\r\n')
-
-def gen3(camera):
-    while True:
-        #get camera frame
-        _,_,_,thresh = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + thresh + b'\r\n\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame3 + b'\r\n\r\n')
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(VideoCamera()),
@@ -44,11 +34,6 @@ def video_feed1():
 @app.route('/video_feed2')
 def video_feed2():
     return Response(gen2(VideoCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/video_feed3')
-def video_feed3():
-    return Response(gen3(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 if __name__ == '__main__':
     # defining server ip address and port
